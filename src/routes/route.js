918ba3e5046fbf396ea1b3b196dfbf0f8bd467b8
dipalidatir-demo <dipalidatir1997@gmail.com>
2022-06-14 07:14:41 +0530

    postapiandmissingnumber

--- src/routes/route.js
+++ src/routes/route.js
@@ -41,6 +41,278 @@ router.get("/test-api-6" , function(req, res) {
     res.send({a:56, b: 45})
 })
 
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
+
+
+router.post("/players", function(req, res) {
+    // const newPlayer = req.body
+    
+    // for (let i = 0; i < players.length; i++) {
+    //     if (players[i].name == newPlayer.name){
+    //       console.log("player exists");
+    //       break;
+    //     }
+    //     if (players[i].name !== newPlayer.name){
+    //         console.log("player is added");
+            
+    //       }
+    // }
+    let newPlayer = req.body
+    let newPlayersName = newPlayer.name
+    let isNameRepeated = false
+
+    //let player = players.find(p => p.name == newPlayersName)
+
+    for(let i = 0; i < players.length; i++) {
+        if(players[i].name == newPlayersName) {
+            isNameRepeated = true;
+            break;
+        }
+    }
+
+    //undefined is same as false/ a falsy value
+    if (isNameRepeated) {
+        //Player exists
+        res.send("This player was already added!")
+    } else {
+        //New entry
+        players.push(newPlayer)
+        res.send(players)
+    }
+});
+
+// missing numbers
+router.get('/sol1', function (req, res) {
+    let arr = [1,2,3,5,6,7]
+    let total = 0;
+    for (var i in arr){
+        total += arr[i];
+    }
+    let lastDigit = arr.pop()
+    let consecutiveSum = lastDigit * (lastDigit+1)/2
+    let missingNumber = consecutiveSum-total
+    res.send( {data: missingNumber})
+});
+
+router.get('/sol2', function (req, res) {
+    let arr = [33, 34, 35, 37, 38]
+    let len = arr.length
+    let total = 0;
+    for (var i in arr){
+        total += arr[i];
+    }
+    let firstDigit = arr.pop[0]
+    let lastDigit = arr.pop()
+    let consecutiveSum = (len + 1) * (firstDigit + lastDigit)/2
+    let missingNumber = consecutiveSum-total
+    res.send( {data: missingNumber})
+});
+
+
+
+
+
+router.post("/test-post", function(req, res) {
+    res.send([ 23, 45 , 6])
+})
+
+
+router.post("/test-post-2", function(req, res) {
+    res.send(  { msg: "hi" , status: true }  )
+})
+
+router.post("/test-post-3", function(req, res) {
+    // let id = req.body.user
+    // let pwd= req.body.password
+
+    // console.log( id , pwd)
+
+    console.log( req.body )
+
+    res.send(  { msg: "hi" , status: true }  )
+})
+
+
+
+router.post("/test-post-4", function(req, res) {
+    let arr= [ 12, "functionup"]
+    let ele= req.body.element
+    arr.push(ele)
+    res.send(  { msg: arr , status: true }  )
+})
+
+module.exports = router;const express = require('express');
+const router = express.Router();
+
+router.get('/students/:name', function(req, res) {
+    let studentName = req.params.name
+    console.log(studentName)
+    res.send(studentName)
+})
+
+router.get("/random" , function(req, res) {
+    res.send("hi there")
+})
+
+
+router.get("/test-api" , function(req, res) {
+    res.send("hi FunctionUp")
+})
+
+
+router.get("/test-api-2" , function(req, res) {
+    res.send("hi FunctionUp. This is another cool API")
+})
+
+
+router.get("/test-api-3" , function(req, res) {
+    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
+})
+
+
+router.get("/test-api-4" , function(req, res) {
+    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+})
+
+
+
+router.get("/test-api-5" , function(req, res) {
+    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+})
+
+router.get("/test-api-6" , function(req, res) {
+    res.send({a:56, b: 45})
+})
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
+
+
+router.post("/players", function(req, res) {
+    // const newPlayer = req.body
+    
+    // for (let i = 0; i < players.length; i++) {
+    //     if (players[i].name == newPlayer.name){
+    //       console.log("player exists");
+    //       break;
+    //     }
+    //     if (players[i].name !== newPlayer.name){
+    //         console.log("player is added");
+            
+    //       }
+    // }
+    let newPlayer = req.body
+    let newPlayersName = newPlayer.name
+    let isNameRepeated = false
+
+    //let player = players.find(p => p.name == newPlayersName)
+
+    for(let i = 0; i < players.length; i++) {
+        if(players[i].name == newPlayersName) {
+            isNameRepeated = true;
+            break;
+        }
+    }
+
+    //undefined is same as false/ a falsy value
+    if (isNameRepeated) {
+        //Player exists
+        res.send("This player was already added!")
+    } else {
+        //New entry
+        players.push(newPlayer)
+        res.send(players)
+    }
+});
+
+// missing numbers
+router.get('/sol1', function (req, res) {
+    let arr = [1,2,3,5,6,7]
+    let total = 0;
+    for (var i in arr){
+        total += arr[i];
+    }
+    let lastDigit = arr.pop()
+    let consecutiveSum = lastDigit * (lastDigit+1)/2
+    let missingNumber = consecutiveSum-total
+    res.send( {data: missingNumber})
+});
+
+router.get('/sol2', function (req, res) {
+    let arr = [33, 34, 35, 37, 38]
+    let len = arr.length
+    let total = 0;
+    for (var i in arr){
+        total += arr[i];
+    }
+    let firstDigit = arr.pop[0]
+    let lastDigit = arr.pop()
+    let consecutiveSum = (len + 1) * (firstDigit + lastDigit)/2
+    let missingNumber = consecutiveSum-total
+    res.send( {data: missingNumber})
+});
+
+
+
+
+
 router.post("/test-post", function(req, res) {
     res.send([ 23, 45 , 6])
 })
